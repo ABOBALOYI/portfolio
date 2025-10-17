4ff3330ef211bff3ad70bf4e418e43084f95962c
Abo Baloyi <abo@baloyiabo.co.za>
2025-10-17 16:08:45 +0200

    Fix React import in index.js to resolve build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Layout, Hero, About, Jobs, Featured, Projects, Contact, Head } from '@components';
